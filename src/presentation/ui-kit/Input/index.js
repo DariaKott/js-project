f4e82764042bff24a95be8b46a15ebeaaa61ce1c
AlinaChertushkina <alina.chertushckina@gmail.com>
2023-09-28 22:02:50 +0300

    added error handler

--- src/presentation/ui-kit/Input/index.js
+++ src/presentation/ui-kit/Input/index.js
@@ -6,7 +6,17 @@ class Input {
   }
 
   onChangeHandler = (e) => {
-    this.onChange(e.target.value);
+    const value = e.target.value;
+
+    // Проверяем введенное значение на наличие опечаток или цифр
+    if (!isNaN(value) || /[a-zA-Z]+\d*/.test(value)) {
+      const errorMessage = document.createElement('p');
+      errorMessage.className = 'error';
+      errorMessage.textContent = ":( Sorry, we don't speak Elvish.Please switch to human";
+      document.body.appendChild(errorMessage);
+    }
+
+    this.onChange(value);
   };
 
   render() {
